05e2326c71c00a6b2f90b39997f152621d80aef1
Zhe Fu <72235751+Matthew-Fu@users.noreply.github.com>
2021-08-25 00:05:47 -0400

    Add files via upload

--- SQL Assignment four.sql
+++ SQL Assignment four.sql
@@ -89,112 +89,165 @@ go
 by the Database Diagram sent to you yesterday. When inserting, make up info if necessary. 
 Write query for each step. Do not use IDE. BE CAREFUL WHEN DELETING DATA OR DROPPING TABLE.*/
 --1.	Lock tables Region, Territories, EmployeeTerritories and Employees. 
-LOCK TABLE Region, Territories, EmployeeTerritories, Employees IN EXCLUSIVE MODE
 --Insert following information into the database. 
 --In case of an error, no changes should be made to DB.
 --a.	A new region called “Middle Earth”;
+
+BEGIN TRAN
+
 select * from Region
-begin tran
-insert into Region (RegionDescription) values ('Middle Earth')
-commit
---b.	A new territory called “Gondor”, belongs to region “Middle Earth”;
 select * from Territories
-begin tran
-insert into Territories (TerritoryDescription, RegionID) values ("Gondor", 5)
-commit
+select * from Employees
+select * from EmployeeTerritories
+
+INSERT INTO Region VALUES(6,'Middel Earth')
+IF @@ERROR <>0
+ROLLBACK
+ELSE BEGIN
+
+--b.	A new territory called “Gondor”, belongs to region “Middle Earth”;
+INSERT INTO Territories VALUES(98105,'Gondor',6)
+DECLARE @error INT  = @@ERROR 
+IF @error <>0
+BEGIN
+PRINT @error
+ROLLBACK
+END
+ELSE BEGIN
 --c.	A new employee “Aragorn King” who's territory is “Gondor”.
-begin tran
-insert into Employees (FirstName, Title, Country) values ("Aragorn", "King", "Gondor")
-commit
+INSERT INTO Employees VALUES('Aragorn',	'King'	,'Sales Representative',	'Ms.'	,'1966-01-27 00:00:00.000','1994-11-15 00:00:00.000', 'Houndstooth Rd.',	'London',	NULL	,'WG2 7LT',	'UK',	'(71) 555-4444'	,452,NULL,	'Anne has a BA degree in English from St. Lawrence College.  She is fluent in French and German.',	5,	'http://accweb/emmployees/davolio.bmp/')
+INSERT INTO EmployeeTerritories VALUES(@@IDENTITY,98105)
+DECLARE @error2 INT  = @@ERROR 
+IF @error2 <>0
+BEGIN
+PRINT @error2
+ROLLBACK
+END
+ELSE BEGIN
+
 --2.	Change territory “Gondor” to “Arnor”.
-begin tran
-update Territories set TerritoryDescription = 'Arnor' where TerritoryDescription = 'Gondor'
-commit
+UPDATE Territories
+SET TerritoryDescription = 'Arnor'
+WHERE TerritoryDescription = 'Gondor'
+IF @@ERROR<>0
+ROLLBACK
+ELSE BEGIN
+
 --3.	Delete Region “Middle Earth”. (tip: remove referenced data first) 
 --(Caution: do not forget WHERE or you will delete everything.) 
 --In case of an error, no changes should be made to DB. Unlock the tables mentioned 
 --in question 1.
-ALTER TABLE Territories
-DROP CONSTRAINT FK_Territories_Region;
-Delete from Region where RegionDescription = 'Middle Earth'
-ALTER TABLE Territories
-ADD CONSTRAINT FK_Territories_Region Foreign key (RegionID) references Region(RegionID);
+DELETE FROM EmployeeTerritories 
+WHERE TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription = 'Arnor')
+DELETE FROM Territories
+WHERE TerritoryDescription = 'Arnor'
+DELETE FROM Region
+WHERE RegionDescription = 'Middel Earth'
+IF @@ERROR <>0
+ROLLBACK
+ELSE BEGIN
+COMMIT
+END
+END
+END
+END
+END
 
 --4.	Create a view named "view_product_order_[your_last_name]", 
 --list all products and total ordered quantity for that product.
-create view view_product_order_Fu as 
+create view view_product_order_Fu 
+as 
 select productID, sum(quantity) as Total_Quantity
 from [Order_details]
 group by productID
 
 --5.	Create a stored procedure “sp_product_order_quantity_[your_last_name]” 
 --that accept product id as an input and total quantities of order as output parameter.
-create proc sp_product_order_quantity_Fu
-@productid int,
-@total_quantities int out
-as
-begin
-select productid, @total_quantities = sum(quantity) 
-from [Order_details]
-where productid = @productid
-group by @productid
-end
+ALTER PROC sp_Product_Order_Quantity_Fu
+@ProductID INT,
+@TotalOrderQty INT OUT
+AS
+BEGIN
+SELECT @TotalOrderQty = SUM(Quantity)  FROM [Order Details] OD JOIN Products P ON P.ProductID = OD.ProductID
+WHERE P.ProductID = @ProductID
+GROUP BY ProductName
+END
+
+DECLARE @Tot INT
+EXEC sp_Product_Order_Quantity_Fu 11,@Tot OUT
+PRINT @Tot 
 
 --6.	Create a stored procedure “sp_product_order_city_[your_last_name]” 
 --that accept product name as an input and top 5 cities that ordered most 
 --that product combined with the total quantity of that product ordered from that city as output.
-create proc sp_product_order_city_Fu
-@productname varchar(20)
-@city varchar(20) OUT
-@total_quantity varchar(20) out
-as
-begin
-select @productname, a.avgPrice, @city = b.ShipCity
-from
-(select Top 5 ProductID, @total_quantity = sum(Quantity), sum(UnitPrice*(1-Discount)*Quantity)/sum(Quantity) as avgPrice
-from [Order Details] where @productname = productname
-group by ProductID
-order by @total_quantity desc) a
-left join
-(select @city = o.ShipCity, od.ProductID, @total_quantity = sum(Quantity), rank() over (partition by od.ProductID order by sum(Quantity) desc) RNK
-from Orders o
-join [Order Details] od on o.orderID = od.OrderID where @productname = productname
-group by @city, od.ProductID) b on a.ProductID = b.ProductID
-where b.RNK = 1 and @productname = a.productname
-end
+ALTER PROC sp_Product_Order_City_Fu
+@ProductName NVARCHAR(50)
+AS
+BEGIN
+SELECT TOP 5 ShipCity,SUM(Quantity) FROM [Order Details] OD JOIN Products P ON P.ProductID = OD.ProductID JOIN Orders O ON O.OrderID = OD.OrderID
+WHERE ProductName=@ProductName
+GROUP BY ProductName,ShipCity
+ORDER BY SUM(Quantity) DESC
+END
+
+
+EXEC sp_Product_Order_City_Fu 'Queso Cabrales'
 
 --7.	Lock tables Region, Territories, EmployeeTerritories and Employees. 
 --Create a stored procedure “sp_move_employees_[your_last_name]” 
 --that automatically find all employees in territory “Tory”; 
 --if more than 0 found, insert a new territory “Stevens Point” of region “North” 
 --to the database, and then move those employees to “Stevens Point”.
-LOCK TABLE Region, Territories, EmployeeTerritories, Employees IN EXCLUSIVE MODE
-create proc sp_move_employees_Fu
-as
-begin
-select e.EmployeeID
-from EmployeeTerritories e join Territories t on e.TerritoryID = t.TerritoryID
-where t.TerritoryDescription = 'Tory'
-insert into Territories (TerritoryDescription, RegionID) values ("StevensPoint", 3)
---???
-end
+BEGIN TRAN
+select * from Region
+select * from Territories
+select * from Employees
+select * from EmployeeTerritories
+GO
+ALTER PROC sp_move_employees_Fu
+AS
+BEGIN
+
+IF EXISTS(SELECT EmployeeID FROM EmployeeTerritories WHERE TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription ='Troy'))
+BEGIN
+DECLARE @TerritotyID INT
+SELECT @TerritotyID = MAX(TerritoryID) FROM Territories
+BEGIN TRAN
+INSERT INTO Territories VALUES(@TerritotyID+1 ,'Stevens Point',3)
+UPDATE EmployeeTerritories
+SET TerritoryID = @TerritotyID+1
+WHERE EmployeeID IN (SELECT EmployeeID FROM EmployeeTerritories WHERE TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription ='Troy'))
+IF @@ERROR <> 0
+BEGIN
+ROLLBACK
+END
+ELSE
+COMMIT
+END
+
+END
+
+EXEC sp_move_employees_Fu
 
 --8.	Create a trigger that when there are more than 100 employees in territory “Stevens Point”, 
 --move them back to Troy. (After test your code,) remove the trigger. 
 --Move those employees back to “Troy”, if any. Unlock the tables.
-create trigger trg_after_100_employee on territories
-after insert
-as 
-begin
-   declare @num int, @add int
-   select @num = count(*) from Territories
-   select @add = count(*) from (select e.EmployeeID
-from EmployeeTerritories e join Territories t on e.TerritoryID = t.TerritoryID
-where t.TerritoryDescription = 'Tory')
-   if @num + @add > 100
-      update Territories set TerritoryDescription = "Troy" where TerritoryDescription = "StevensPoint"
-      delete trg_after_100_employee
-end
+CREATE TRIGGER tr_move_emp_Fu
+ON EmployeeTerritories
+AFTER INSERT
+AS
+DECLARE @EmpCount INT
+SELECT @EmpCount = COUNT(*) FROM EmployeeTerritories WHERE TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription = 'Stevens Point' AND RegionID=3) GROUP BY EmployeeID
+IF (@EmpCount>100)
+BEGIN
+UPDATE EmployeeTerritories
+SET TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription ='Troy')
+WHERE EmployeeID IN (SELECT EmployeeID FROM EmployeeTerritories WHERE TerritoryID = (SELECT TerritoryID FROM Territories WHERE TerritoryDescription ='Stevens Point' AND RegionID=3))
+END
+
+DROP TRIGGER tr_move_emp_Fu
+
+COMMIT
 
 --9.	Create 2 new tables “people_your_last_name” “city_your_last_name”. 
 --City table has two records: {Id:1, City: Seattle}, {Id:2, City: Green Bay}. 
@@ -204,16 +257,44 @@ end
 --Create a view “Packers_your_name” lists all people from Green Bay. 
 --If any error occurred, no changes should be made to DB. (after test) 
 --Drop both tables and view.
-create table people_your_Fu (id int, Name varchar(20), City int)
-create table city_your_Fu (id int, name varchar(20))
-insert into city_your_Fu values (1, 'Seattle'), (2, 'Green Bay')
-insert into people_your_Fu values (1, 'Aaron Rodgers', 2), (2, 'Russell Wilson', 1), (3, 'Jody Nelson', 2)
-update city_your_Fu set City = 'Madison' where City = 'Seattle'
-create view Packers_Fu as
-select people_your_Fu.ID from people_your_Fu
-where people_your_Fu.city = city_your_Fu.id
-Drop table city_your_Fu
-drop table people_your_Fu
+CREATE TABLE People_Fu
+(
+id int ,
+name nvarchar(100),
+city int
+)
+
+create table City_Fu
+(
+id int,
+city nvarchar(100)
+)
+BEGIN TRAN 
+insert into City_Fu values(1,'Seattle')
+insert into City_Fu values(2,'Green Bay')
+
+insert into People_Fu values(1,'Aaron Rodgers',1)
+insert into People_Fu values(2,'Russell Wilson',2)
+insert into People_Fu values(3,'Jody Nelson',2)
+
+if exists(select id from People_Fu where city = (select id from City_Fu where city = 'Seatle'))
+begin
+insert into City_Fu values(3,'Madison')
+update People_Fu
+set city = 'Madison'
+where id in (select id from People_Fu where city = (select id from City_Fu where city = 'Seatle'))
+end
+delete from City_Fu where city = 'Seattle'
+
+CREATE VIEW Packers_Fu
+AS
+SELECT name FROM People_Fu WHERE city = 'Green Bay'
+
+select * from Packers_Fu
+commit
+drop table People_Fu
+drop table City_Fu
+drop view Packers_Fu
 
 --10.	 Create a stored procedure “sp_birthday_employees_[you_last_name]” 
 --that creates a new table “birthday_employees_your_last_name” 
@@ -222,9 +303,9 @@ drop table people_your_Fu
 create proc sp_birthday_employees_Fu
 as
 begin
-create table birthday_employees_Fu (identity(1,1) primary key, Name)
-insert into birthday_employees_Fu values (
-select LastName + ' ' + FirstName as [name] from Employees where month(birthdate) = 2)
+SELECT * INTO #EmployeeTemp
+FROM Employees WHERE DATEPART(MM,BirthDate) = 02
+SELECT * FROM #EmployeeTemp
 end
 
 --11.	Create a stored procedure named “sp_your_last_name_1” that returns all cites 
@@ -254,10 +335,42 @@ group by c.CustomerID, od.ProductID, C.City
 having count(od.ProductID) <= 1 and count(c.CustomerID) >= 2
 end
 
+CREATE PROC sp_Fu_1
+AS
+BEGIN
+SELECT City FROM CUSTOMERS
+GROUP BY City
+HAVING COUNT(*)>2
+INTERSECT
+SELECT City FROM Customers C JOIN Orders O ON O.CustomerID=C.CustomerID JOIN [Order Details] OD ON O.OrderID = OD.OrderID
+GROUP BY OD.ProductID,C.CustomerID,City
+HAVING COUNT(*) BETWEEN 0 AND 1
+END
+GO
+EXEC sp_Fu_1
+GO
+CREATE PROC sp_Fu_2
+AS
+BEGIN
+SELECT City FROM CUSTOMERS
+WHERE CITY IN (SELECT City FROM Customers C JOIN Orders O ON O.CustomerID=C.CustomerID JOIN [Order Details] OD ON O.OrderID = OD.OrderID
+GROUP BY OD.ProductID,C.CustomerID,City
+HAVING COUNT(*) BETWEEN 0 AND 1)
+GROUP BY City
+HAVING COUNT(*)>2
+END
+GO
+EXEC sp_Fu_2
+GO
+
 --12.	How do you make sure two tables have the same data?
 SELECT * FROM Table1
 UNION
 SELECT * FROM Table2
+
+SELECT * FROM Customers
+EXCEPT
+SELECT * FROM Customers
 
 /*14. First Name	Last Name	Middle Name
 John	Green	
